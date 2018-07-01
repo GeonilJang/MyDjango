ddf0c9c2370692425610de92a24a1996f70d4fe6
GeonilJang <jgi0105@naver.com>
2018-07-02 04:33:06 +0900

    ep15)부트스트렙 적용

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     url(r'^$', views.post_list, name="post_list"),
     url(r'^(?P<id>\d+)/$', views.post_detail, name="post_detail"),
 
-    url(r'^cbv/new/$', views_cbv.post_new )
+    url(r'^cbv/new/$', views_cbv.post_new, name="post_new" )
 ]
