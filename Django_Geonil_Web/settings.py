6d8ae60dd21fd67d99a9cd0938aa50b3b118ea77
GeonilJang <jgi0105@naver.com>
2018-07-04 20:19:39 +0900

    static finder 설정 및 환경 설정

--- Django_Geonil_Web/settings.py
+++ Django_Geonil_Web/settings.py
@@ -68,7 +68,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Django_Geonil_Web.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
@@ -78,8 +77,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
 
@@ -97,23 +94,20 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+#정적인 파일을 접근 할 수 있도록 설정하기
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'Django_Geonil_Web', 'static')
+]
